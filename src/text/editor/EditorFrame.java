3fb4bbd7baca3eef10357fd6d27bf95ad6f68b37
cvota <cvota@DESKTOP-AUT7SC1>
2019-02-02 12:58:07 -0600

    Uploading Basic Text Editor Through NetBeans

---
+++ src/text/editor/EditorFrame.java
@@ -0,0 +1,159 @@
+/* Editor Created by Christopher Lawton */
+
+package text.editor;
+
+import java.awt.Color;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class EditorFrame extends javax.swing.JFrame {
+
+
+    public EditorFrame() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        textField = new javax.swing.JTextArea();
+        clearBtn = new javax.swing.JButton();
+        fileName = new javax.swing.JTextField();
+        saveBtn = new javax.swing.JButton();
+        getBtn = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Chris's Text Editor");
+
+        textField.setColumns(20);
+        textField.setRows(5);
+        jScrollPane1.setViewportView(textField);
+
+        clearBtn.setText("Clear");
+        clearBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clearBtnActionPerformed(evt);
+            }
+        });
+
+        saveBtn.setText("Save");
+        saveBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                saveBtnActionPerformed(evt);
+            }
+        });
+
+        getBtn.setText("Get");
+        getBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                getBtnActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(fileName, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(getBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(saveBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(clearBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 278, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(fileName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(clearBtn)
+                    .addComponent(saveBtn)
+                    .addComponent(getBtn))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void clearBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearBtnActionPerformed
+        textField.setForeground(Color.black);
+        textField.setText("");
+        fileName.setText("");
+    }//GEN-LAST:event_clearBtnActionPerformed
+
+    private void getBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_getBtnActionPerformed
+
+        String text = null;
+        String file = "";
+        
+        try(
+            BufferedReader in = new BufferedReader(
+                                new FileReader(fileName.getText()))) {
+            text = in.readLine();
+            while ((file = in.readLine()) != null){
+                //file.concat(in.readLine() + "\n");
+                text = text + "\n" + file;
+            }
+            textField.setForeground(Color.black);
+            textField.setText(text);
+            in.close();
+        } catch (IOException e) {
+            textField.setForeground(Color.red);
+            textField.setText("Error: File not found!");
+            return;
+        } 
+    }//GEN-LAST:event_getBtnActionPerformed
+
+    private void saveBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveBtnActionPerformed
+        try{
+            BufferedWriter out = new BufferedWriter (
+                                 new FileWriter (fileName.getText()));
+
+            textField.setForeground(Color.black);
+            for (String retval: textField.getText().split("\n")) {
+                out.write(retval);
+                out.newLine();
+            }
+
+            out.close(); //closing the stream flushes the buffer
+
+        } catch (IOException e) {
+            textField.setForeground(Color.red);
+            textField.setText("Error: Specify a filename.");
+        }
+    }//GEN-LAST:event_saveBtnActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new EditorFrame().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton clearBtn;
+    private javax.swing.JTextField fileName;
+    private javax.swing.JButton getBtn;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton saveBtn;
+    private javax.swing.JTextArea textField;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/text/editor/TextEditor.java
@@ -0,0 +1,12 @@
+/* Text EditorFrame form created with Netbeans GUI Editor */
+
+package text.editor;
+
+public class TextEditor {
+
+    public static void main(String[] args) {
+        EditorFrame textEditor = new EditorFrame();
+        textEditor.setVisible(true);
+    }
+    
+}
